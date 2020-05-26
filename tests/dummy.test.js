358c783dc7d5c180fcebf72b4514c9d19da40dd7
DevYojan <devyojan@gmail.com>
2020-05-26 21:58:42 +0400

    tests for favoriteblog and total likes

--- tests/dummy.test.js
+++
@@ -1,8 +0,0 @@
-const listHelper = require('../utils/list_helper');
-
-describe('dummy', () => {
-	test('returns 1', () => {
-		const blogs = [];
-		expect(listHelper.dummy(blogs)).toBe(1);
-	});
-});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,23 @@ const dummy = (blogs) => {
 	return 1;
 };
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+	let sum = 0;
+
+	for (blog of blogs) {
+		sum += blog.likes;
+		debugger;
+	}
+
+	return sum;
+};
+
+const favoriteBlog = (blogs) => {
+	const blogWithMaxLikes = blogs.reduce((prev, current) => {
+		return prev.likes > current.likes ? prev : current;
+	});
+
+	return blogWithMaxLikes;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog };
