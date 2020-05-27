270d584c2f4d75f9746470388d1e6755e87d939a
DevYojan <devyojan@gmail.com>
2020-05-27 21:42:00 +0400

    mostblogs with test

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -90,6 +90,11 @@ describe('total Likes', () => {
 });
 
 describe('favorite Blog', () => {
+	test('returns a empty object if the blog array is empty', () => {
+		const blogs = [];
+		expect(listHelper.favoriteBlog(blogs)).toEqual({});
+	});
+
 	test('is a single blog if a single blog is passed', () => {
 		const blogs = [
 			{
@@ -108,6 +113,100 @@ describe('favorite Blog', () => {
 	});
 
 	test('returns a blog with highest likes if passed many blogs.', () => {
+		const blogs = [
+			{
+				_id: '5a422a851b54a676234d17f7',
+				title: 'React patterns',
+				author: 'Michael Chan',
+				url: 'https://reactpatterns.com/',
+				likes: 7,
+				__v: 0,
+			},
+			{
+				_id: '5a422aa71b54a676234d17f8',
+				title: 'Go To Statement Considered Harmful',
+				author: 'Edsger W. Dijkstra',
+				url:
+					'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+				likes: 5,
+				__v: 0,
+			},
+			{
+				_id: '5a422b3a1b54a676234d17f9',
+				title: 'Canonical string reduction',
+				author: 'Edsger W. Dijkstra',
+				url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+				likes: 12,
+				__v: 0,
+			},
+			{
+				_id: '5a422b3a1b54a676234d17f9',
+				title: 'Canonical string reduction',
+				author: 'Edsger W. Dijkstra',
+				url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+				likes: 12,
+				__v: 0,
+			},
+			{
+				_id: '5a422b891b54a676234d17fa',
+				title: 'First class tests',
+				author: 'Robert C. Martin',
+				url:
+					'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+				likes: 10,
+				__v: 0,
+			},
+			{
+				_id: '5a422ba71b54a676234d17fb',
+				title: 'TDD harms architecture',
+				author: 'Robert C. Martin',
+				url:
+					'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+				likes: 0,
+				__v: 0,
+			},
+			{
+				_id: '5a422bc61b54a676234d17fc',
+				title: 'Type wars',
+				author: 'Robert C. Martin',
+				url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+				likes: 2,
+				__v: 0,
+			},
+		];
+
+		const result = listHelper.favoriteBlog(blogs);
+
+		expect(result).toEqual(blogs[2]);
+	});
+});
+
+describe('Author with highest number of blogs', () => {
+	test('is empty object if blog array is empty', () => {
+		const blogs = [];
+		expect(listHelper.mostBlogs(blogs)).toEqual({});
+	});
+
+	test('if there is only a single blog a single author with a number 1', () => {
+		const blogs = [
+			{
+				_id: '5a422a851b54a676234d17f7',
+				title: 'React patterns',
+				author: 'Michael Chan',
+				url: 'https://reactpatterns.com/',
+				likes: 7,
+				__v: 0,
+			},
+		];
+
+		const result = listHelper.mostBlogs(blogs);
+		expect(result).toEqual({
+			author: 'Michael Chan',
+			blogs: 1,
+		});
+	});
+
+	test('gives correct result if many blogs are passed', () => {
 		const blogs = [
 			{
 				_id: '5a422a851b54a676234d17f7',
@@ -162,8 +261,9 @@ describe('favorite Blog', () => {
 			},
 		];
 
-		const result = listHelper.favoriteBlog(blogs);
-
-		expect(result).toEqual(blogs[2]);
+		expect(listHelper.mostBlogs(blogs)).toEqual({
+			author: 'Robert C. Martin',
+			blogs: 3,
+		});
 	});
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -14,6 +14,10 @@ const totalLikes = (blogs) => {
 };
 
 const favoriteBlog = (blogs) => {
+	if (blogs.length === 0) {
+		return {};
+	}
+
 	const blogWithMaxLikes = blogs.reduce((prev, current) => {
 		return prev.likes > current.likes ? prev : current;
 	});
@@ -21,4 +25,36 @@ const favoriteBlog = (blogs) => {
 	return blogWithMaxLikes;
 };
 
-module.exports = { dummy, totalLikes, favoriteBlog };
+const mostBlogs = (blogs) => {
+	if (blogs.length === 0) {
+		return {};
+	}
+
+	let blogAuthors = {};
+
+	for (blog of blogs) {
+		if (!blogAuthors.hasOwnProperty(blog.author)) {
+			blogAuthors[blog.author] = 1;
+
+			continue;
+		}
+
+		blogAuthors[blog.author] += 1;
+	}
+
+	let authorWithMaxBlogs = {
+		author: '',
+		blogs: 0,
+	};
+
+	for (let [author, noOfBlogs] of Object.entries(blogAuthors)) {
+		if (noOfBlogs > authorWithMaxBlogs.blogs) {
+			authorWithMaxBlogs.author = author;
+			authorWithMaxBlogs.blogs = noOfBlogs;
+		}
+	}
+
+	return authorWithMaxBlogs;
+};
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs };
